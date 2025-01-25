023c0ac050614bb45efe2d882f16cb35f201221d
Adi <adihakanyan7@gmail.com>
2025-01-25 08:59:50 +0200

    Fix MongoDB connection and update db.js

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log('MongoDB Connection Success');
+    }
+    catch (err) {
+        console.error('MongoDB Connection Failed:', err.message);
+        console.log('Stack trace:', err.stack);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
